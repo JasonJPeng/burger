df9a02f13d79f7d72a06b90999031929ca93ad4d
Jason Peng <jason.j.peng@gamil.com>
2019-07-23 22:10:19 -0700

    funtional

---
+++ server.js
@@ -0,0 +1,98 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "12345678",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Root get route
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+      if (err) throw err;
+  
+      // Test it
+      // console.log('The solution is: ', data);
+  
+      // Test it
+      // return res.send(data);
+  
+      res.render("index", { all: data });
+    });
+  });
+  
+  // Post route -> back to home
+  app.post("/api/movies", function(req, res) {
+    
+    connection.query("INSERT INTO wishes (wish) VALUES (?)", [req.body.wish], function(err, result) {
+      if (err) throw err;
+  
+      res.redirect("/");
+    });
+  });
+
+
+  // Delete a plan
+app.delete("/api/movies/:id", function(req, res) {
+  connection.query("DELETE FROM wishes WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+  
+// Update a plan
+app.put("/api/movie/:id", function(req, res) {
+  connection.query("UPDATE wishes SET wish = ? WHERE id = ?", [req.body.newMov, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+
+  // Start our server so that it can begin listening to client requests.
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
